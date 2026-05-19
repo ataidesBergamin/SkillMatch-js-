2a5684ca614b2666c62074c69528a04cd5fb9c45
ataidesBergamin <ataidesnatabergamin@gmail.com>
2026-05-19 16:23:49 -0300

    commit-3
    feat: implantação cálculo de compatibilidade

--- skillmatch.js
+++ skillmatch.js
@@ -1,4 +1,4 @@
-// Objeto de Candidato
+﻿// Objeto de Candidato
 const candidato = {
   nome: "Pedro Silva",
   area: "Front-end",
@@ -11,7 +11,7 @@ const vagas = [
   {
     id: 1,
     empresa: "Tech Master",
-    cargo: "Desenvolvedor Front-end Júnior",
+    cargo: "Desenvolvedor Front-end Junior",
     requisitos: ["JavaScript", "React", "Node.js"],
     salario: 5000,
     modalidade: "Presencial",
@@ -19,36 +19,62 @@ const vagas = [
   {
     id: 2,
     empresa: "New Tech",
-    cargo: "Desenvolvedor Front-end Júnior",
-    requisitos: ["JavaScript", "CSS", "HTML5", "Logica de Programação"],
-    salario: 3200,
-    modalidade: "Híbrida",
+    cargo: "Desenvolvedor Front-end Junior",
+    requisitos: ["JavaScript", "CSS", "HTML5", "Logica de Programacao"],
+    salario: 3600,
+    modalidade: "Hibrida",
   },
   {
     id: 3,
-    empresa: "Inova Solucões",
-    cargo: "Desenvolvedor Front-end Júnior",
-    requisitos: ["CSS", "HTML5", "Logica de Programação", "GitHub"],
-    salario: 5000,
+    empresa: "Inova Solucoes",
+    cargo: "Desenvolvedor Front-end Junior",
+    requisitos: ["CSS", "HTML5", "Logica de Programacao", "GitHub"],
+    salario: 2800,
     modalidade: "Remota",
   },
   {
     id: 4,
     empresa: "Exelencia Softwares",
-    cargo: "Desenvolvedor Front-end Júnior",
-    requisitos: ["JavaScript", "React", "GitHub", "Inglês Intermediário"],
+    cargo: "Desenvolvedor Front-end Junior",
+    requisitos: ["JavaScript", "React", "GitHub", "Ingles Intermediario"],
     salario: 8000,
-    modalidade: "Híbrida",
+    modalidade: "Hibrida",
   },
 ];
 
-/* class skills {
-  constructor() {
-    this.habilidades = [];
-    this.requisitos = [];
+// Calcular quais vagas sao compativeis com as habilidades do candidato
+
+function calcularCompatibilidade(candidato, vagas) {
+  // recebe as habilidades do candidato ou seja o arry:["JavaScript", "React", "Node.js"]
+  const habilidadesCandidato = candidato.habilidades;
+  // array para armazenar as vagas que sao  100% compativeis com o candidato
+  const vagasCompativeis = [];
+
+  for (const vaga of vagas) {
+    let requisitosAtendidos = 0;
+
+    for (const requisito of vaga.requisitos) {
+      if (habilidadesCandidato.includes(requisito)) {
+        requisitosAtendidos++;
+      }
+    }
+
+    if (requisitosAtendidos === vaga.requisitos.length) {
+      vagasCompativeis.push(vaga);
+    }
   }
-  addSkill(skill) {
-    this.habilidades.push(skill);
-    this.requisitos.push(skill);
-  }
-} */
+
+  return vagasCompativeis;
+}
+
+const vCompativeis = calcularCompatibilidade(candidato, vagas);
+
+console.log("Vagas compativeis para o candidato " + candidato.nome + ":");
+
+for (const vaga of vCompativeis) {
+  console.log("- " + vaga.empresa + " | " + vaga.cargo);
+}
+
+if (vCompativeis.length === 0) {
+  console.log("Nenhuma vaga totalmente compativel encontrada.");
+}
