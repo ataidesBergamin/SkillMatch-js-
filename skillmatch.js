2918a06d96b3bf15b4ec0aa41ae3c6826ead52c1
ataidesBergamin <ataidesnatabergamin@gmail.com>
2026-05-19 13:16:07 -0300

    commit-2
    feat: adiciona dados do candidato e vagas

--- skillmatch.js
+++ skillmatch.js
@@ -1,6 +1,54 @@
-class {
-    constructor() {
-        this.skills = [];
-        this.requisites = [];
-    }
-}
+// Objeto de Candidato
+const candidato = {
+  nome: "Pedro Silva",
+  area: "Front-end",
+  habilidades: ["JavaScript", "React", "Node.js"],
+  tempoExperienciaMeses: 3,
+};
+
+// Array de Vagas
+const vagas = [
+  {
+    id: 1,
+    empresa: "Tech Master",
+    cargo: "Desenvolvedor Front-end Júnior",
+    requisitos: ["JavaScript", "React", "Node.js"],
+    salario: 5000,
+    modalidade: "Presencial",
+  },
+  {
+    id: 2,
+    empresa: "New Tech",
+    cargo: "Desenvolvedor Front-end Júnior",
+    requisitos: ["JavaScript", "CSS", "HTML5", "Logica de Programação"],
+    salario: 3200,
+    modalidade: "Híbrida",
+  },
+  {
+    id: 3,
+    empresa: "Inova Solucões",
+    cargo: "Desenvolvedor Front-end Júnior",
+    requisitos: ["CSS", "HTML5", "Logica de Programação", "GitHub"],
+    salario: 5000,
+    modalidade: "Remota",
+  },
+  {
+    id: 4,
+    empresa: "Exelencia Softwares",
+    cargo: "Desenvolvedor Front-end Júnior",
+    requisitos: ["JavaScript", "React", "GitHub", "Inglês Intermediário"],
+    salario: 8000,
+    modalidade: "Híbrida",
+  },
+];
+
+/* class skills {
+  constructor() {
+    this.habilidades = [];
+    this.requisitos = [];
+  }
+  addSkill(skill) {
+    this.habilidades.push(skill);
+    this.requisitos.push(skill);
+  }
+} */
